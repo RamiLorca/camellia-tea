baa7c9dd999884d2b7029e93d59675b985abe196
Rami Lorca <ramilorca@Ramis-MBP.cable.rcn.com>
2023-08-08 14:08:38 -0400

    styled tea cards

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
 
 const App = () => {
 
-  const categories = [
-    {
-      "id": 1,
-      "title": "Black",
-      "imageUrl": "https://images.unsplash.com/photo-1627828094454-accc9a7c20e9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1180&q=80"
-    },
-    {
-      "id": 2,
-      "title": "Green",
-      "imageUrl": "https://images.unsplash.com/photo-1582793988951-9aed5509eb97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1771&q=80"
-    },
-    {
-      "id": 3,
-      "title": "White",
-      "imageUrl": "https://images.unsplash.com/photo-1611836579732-d4dd63dc5492?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2206&q=80"
-    },
-    {
-      "id": 4,
-      "title": "Oolong",
-      "imageUrl": "https://images.unsplash.com/photo-1470162656305-6f429ba817bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
-    },
-    {
-      "id": 5,
-      "title": "Teaware",
-      "imageUrl": "https://images.unsplash.com/photo-1607644962019-09feffe09624?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1774&q=80"
-    }
-  ];
-
-  return <Directory categories={ categories } />;
-
+  return (
+    <Routes>
+      <Route path='/' element={ <Home /> } />
+    </Routes>
+  ) 
 };
 
 export default App;
