b3b6802d417061e43e2d01074d715039539692a4
Rami Lorca <ramilorca@Ramis-MBP.cable.rcn.com>
2023-08-12 19:31:37 -0400

    checkpoint: checkout menu and shop items

--- src/index.js
+++ src/index.js
@@ -5,20 +5,22 @@ import App from './App';
 import { UserProvider } from './context/user.context';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ProductsProvider } from './context/products.context';
+import { CartProvider } from './context/cart.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
 reportWebVitals();
